73a1cd5f2f79f3833587ab56751e6d0780c8e1a6
ueb1she <anotov2002116@mail.ru>
2021-12-24 03:21:35 +0300

    Initial commit

---
+++ src/main/java/se/itmo/checkpointsbackend/CheckPointsBackendApplication.java
@@ -0,0 +1,13 @@
+package se.itmo.checkpointsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckPointsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckPointsBackendApplication.class, args);
+    }
+
+}
